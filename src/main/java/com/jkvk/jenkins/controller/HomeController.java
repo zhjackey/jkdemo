c3314036693c1c45b6ae9fffe9ae0e0e98e7cdea
zhjackey <zhjackey@outlook.com>
2020-11-13 16:05:36 +0800

    replace Home to home

--- src/main/java/com/jkvk/jenkins/controller/HomeController.java
+++ src/main/java/com/jkvk/jenkins/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version: 1.0
  */
 @RestController
-@RequestMapping("/Home")
+@RequestMapping("/home")
 public class HomeController {
 
     @GetMapping("/index")
